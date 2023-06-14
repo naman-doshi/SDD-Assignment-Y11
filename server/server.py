7d711fc4eecbc5d13b1d70ab3ed0a3e067697214
aerobull42 <doshinaman2006@gmail.com>
2023-06-14 19:10:31 +1000

    initial commit

---
+++ server/server.py
@@ -0,0 +1,202 @@
+# Importing dependencies
+from flask import Flask, jsonify, request, render_template
+import json
+import random
+from bardapi import Bard
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+# Function to get parsed Bard questions
+def askBard(string):
+    
+    # Setting up variables for API key shuffling in case rate limits are reached 
+    numberOfAPI = 4
+    apiNumber = numberOfAPI
+
+    # Initialising Bard instance
+    token = os.getenv("BARD_0")
+    bard = Bard(token=token)
+
+    # Repeatedly trying to get a response until it is successful
+    ans = None
+    while ans == None:
+        try:
+            ans = bard.get_answer(string)['content']
+        except:
+            # If the rate limit is reached, the API key is changed in a cyclical manner
+            apiNumber += 1
+            if apiNumber == numberOfAPI:
+                apiNumber = 0
+            token = os.getenv("BARD_" + str(apiNumber))
+            bard = Bard(token=token)
+
+    # Splitting the response into lines
+    ans = ans.splitlines()
+
+    # Deleting the first line (always useless) since it may trigger false positives in question detection
+    del ans[0]
+
+    questions = []
+
+    for line in ans:
+        # Checking if the line actually contains something of use (i.e. not blank)
+        if len(line) > 5:
+            # Checking if the line is a question
+            if line[0].isnumeric():
+                questions.append([line[3:]])
+            # Checking if it's an answer option
+            elif line[0] == '(':
+                questions[-1].append(line[4:])
+            # Checking if it's a correct answer in the format "The answer is (A)" just in case
+            elif '(' in line and ')' in line and len(questions[0]) > 3:
+                questions[-1].append(line[line.index('(')+1:line.index(')')])
+            # Checking if it's a correct answer in the format "Answer: A"
+            elif "answer:" in line.lower():
+                questions[-1].append(line[line.index(':')+2])
+
+    return questions
+
+def loadFileQuestions(numQuestions):
+
+    # Initialising an integer that counts how many lines have been loaded so far
+    numLinesLoaded = 0
+
+    # Initialising an integer that stores the current question number (0-indexed)
+    currentQuestion = 0
+
+    # Initialising an array that will store all of the questions and will ultimately be returned
+    allQuestions = []
+
+    # Opening the questions file and iterating over each line
+    # Since the "with" method is used, the file is automatically closed
+    with open('server/questions.txt', 'r') as questionFile:
+        # Reads the first line of the file, and strips it of any newline characters or spaces
+        currentLine = questionFile.readline().strip()
+
+        # Pre-test loop — checks if the current line is not blank
+        while currentLine and numLinesLoaded < numQuestions * 6:
+
+            # If the number of lines loaded is divisible by 6, a new question must have started
+            # This is because each question has 6 lines
+            if numLinesLoaded % 6 == 0:
+                # Incrementing the current question number and appending a new empty array to the list of questions
+                # This signifies the start of a new question
+                currentQuestion = int(numLinesLoaded / 6)
+                allQuestions.append([])
+            
+            # Appending another line of the text file to the current question
+            allQuestions[currentQuestion].append(currentLine.strip())
+
+            # Incrementing the number of lines loaded
+            numLinesLoaded += 1
+
+            # Reading the next line
+            currentLine = questionFile.readline().strip()
+
+    # Randomising the order of the questions
+    random.shuffle(allQuestions)
+    doublyRandomisedQuestions = []
+    letterToIndex = {'A': 1, 'B': 2, 'C':3, 'D':4}
+    i = 0
+    
+    # Randomising the order of the answer options and appending the correct answer's text to the end of the question
+    # Also accounts for the edge case where the user requests 10 questions
+    while i < min(20, numQuestions):
+        currentQuestion = allQuestions[i]
+        question = currentQuestion[0]
+        correctAnswerText = currentQuestion[letterToIndex[currentQuestion[5]]]
+        options = currentQuestion[1:5]
+        random.shuffle(options)
+        doublyRandomisedQuestions.append([question] + options + [correctAnswerText])
+        i += 1
+    
+    # If the user requests more than 20 questions, the Bard API is used to generate the rest
+    if numQuestions > 20:
+        doublyRandomisedQuestions += loadBardQuestions(numQuestions - 20)
+
+    return doublyRandomisedQuestions
+
+def loadBardQuestions(numQuestions):
+    # Asking questions 10 at a time — the maximum that Bard can reliably provide in one answer
+    i = 0
+    questions = []
+    while i < numQuestions:
+        questions += askBard(f"Please provide 10 moderately difficult multiple choice questions, 4 answer options (formatted as (A), (B), (C), (D)), and the correct answer (a new line formatted exactly like Answer: D) for the New South Wales Software Design and Development syllabus. Please respond with no formatting, and give me a variety of questions covering different aspects of the syllabus.")
+        i += 10
+
+
+    # Randomising the order of the answer options and appending the correct answer's text to the end of the question
+    letterToIndex = {'A': 1, 'B': 2, 'C':3, 'D':4}
+    newQuestions = []
+
+    for item in questions:
+        question = item[0]
+        correctAnswerText = item[letterToIndex[item[5]]]
+        options = item[1:5]
+        random.shuffle(options)
+        newQuestions.append([question] + options + [correctAnswerText])
+        
+    return newQuestions
+
+
+# Initialising Flask app with directories
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
+
+# Flask route to render index.html at all times when a user is viewing it (via a GET request).
+@app.route('/', methods = ['GET'])
+def index():
+    return render_template(("index.html"))
+
+# Method to return questions to the front end
+@app.route('/getQuestions', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        # Processing the data from the frontend
+        file = request.form
+        params = file['params']
+        params = json.loads(params)
+
+        # Getting the variables numQuestions and loadFileQuestions (LFQ)
+        numQuestions = int(params['numQuestions'][0])
+        LFQ = params['LFQ'][0]
+        
+        # Acquiring the questions
+        if LFQ == True:
+            allQuestions = loadFileQuestions(numQuestions)
+        else:
+            allQuestions = loadBardQuestions(numQuestions)
+        
+        # Returning the questions in JSON format
+        return jsonify(allQuestions)
+
+# Method to log the user's score and other metrics       
+@app.route('/conclude', methods=['POST', 'GET'])
+def conclude():
+    if request.method == 'POST':
+        # Processing the data from the frontend
+        file = request.form
+        params = file['params']
+        params = json.loads(params)
+        playerScore = int(params['playerScore'][0])
+        correctAnswers = int(params['correctAnswers'][0])
+        questionsAsked = int(params['questionsAsked'][0])
+
+        # Same as Version 7 — Writing the game's statistics to the report
+        with open("server/report.txt", 'a') as userReport:
+            # Appending text to indicate the start of a new report
+            userReport.write("————— Commence New Report —————\n")
+
+            # Appending the normal metrics displayed to the user as well
+            userReport.write("GAME OVER!\n")
+            userReport.write(f"Thank you for playing Who Wants to Be a University Student!\n")
+            userReport.write(f"Your final score was {playerScore} points.\n")
+            userReport.write(f"You answered {correctAnswers} out of {questionsAsked} questions correctly.\n")
+
+            # Returning '0' only because it is required; it has no real purpose
+            return '0'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
---
+++ static/script.js
@@ -0,0 +1,196 @@
+// Creating a canvas for the confetti
+const canvas = document.getElementById("confettiBox");
+const jsConfetti = new JSConfetti({ canvas });
+
+// Adding an event listener to trigget the logData function when the form is submitted
+let form = document.getElementById("game-settings");
+form.addEventListener("submit", logData);
+
+// Function to update the text of each question
+function updateQuestion(list) {
+  // Create a variable for each element in the question
+  let question = document.getElementById("questionText");
+  let optA = document.getElementById("optionAChoice");
+  let optB = document.getElementById("optionBChoice");
+  let optC = document.getElementById("optionCChoice");
+  let optD = document.getElementById("optionDChoice");
+
+  // Update the text of each element
+  question.innerHTML = list[0];
+  optA.innerHTML = list[1];
+  optB.innerHTML = list[2];
+  optC.innerHTML = list[3];
+  optD.innerHTML = list[4];
+}
+
+// Function to double the score when the user answers a question correctly
+function doubleScore() {
+  let score = document.getElementById("score");
+  score.innerHTML = parseInt(score.innerHTML) * 2;
+}
+
+// Function to reset the score to 1 when the user gets a question wrong
+function resetScore() {
+  let score = document.getElementById("score");
+  score.innerHTML = 1;
+}
+
+// Function to estimate the waiting time for the user
+function estimateWaitingTime(numQuestions, LFQ) {
+  let time = 0;
+  let timeBlock = document.getElementById("wait-time");
+
+  // From general testing, each question takes about 0.9 seconds to be generated.
+  if (LFQ) {
+    // Of course, we need to subtract the time taken to generate the pre-written questions.
+    // This works every time except when numQuestions = 10
+    // But in this case, the actual loading time will be 0 seconds and thus this page will not be shown to the user.
+    time = 0.9 * Math.abs(numQuestions - 20);
+  } else {
+    time = 0.9 * numQuestions;
+  }
+
+  timeBlock.innerHTML = time;
+}
+
+// Function to make the intro page disappear and the loading page appear
+function showLoadingPage(numQuestions, LFQ) {
+  let f = document.getElementById("question-box");
+  f.classList.add("hidden");
+  f.classList.remove("shown");
+
+  let elem = document.getElementById("loading");
+  elem.classList.add("shown");
+  elem.classList.remove("hidden");
+
+  estimateWaitingTime(numQuestions, LFQ);
+}
+
+// Function to log the data from the form to the backend and start the game
+function logData(event) {
+  // Prevent the page from automatically reloading on form submission
+  event.preventDefault();
+
+  // Hiding the intro page
+  let f = document.getElementById("question-box");
+  f.classList.add("hidden");
+  f.classList.remove("shown");
+
+  // Creating the request
+  let req = new XMLHttpRequest();
+  let formData = new FormData();
+
+  // Creating the parameters to be passed
+  let params = {
+    numQuestions: [numQuestions.value],
+    LFQ: [includePreWrittenQuestions.checked],
+  };
+
+  // Appending the parameters to the form data
+  formData.append("params", JSON.stringify(params));
+
+  // Showing the loading page
+  showLoadingPage(numQuestions.value, includePreWrittenQuestions.checked);
+
+  // Setting a function to be called when the request is complete
+  req.onreadystatechange = function () {
+    if (req.readyState == XMLHttpRequest.DONE) {
+      // Get the questions from the backend, and start the game with these question
+      var allQuestions = req.responseText;
+      startGame(JSON.parse(allQuestions));
+    }
+  };
+
+  // Sending the request to the endpoint /getQuestions
+  req.open("POST", "/getQuestions");
+  req.send(formData);
+}
+
+// Tedious function to show the ending page
+function showEndingPage(correctAnswers, totalQuestions) {
+  // Showing the ending page and updating all variables in it
+  let finalScore = document.getElementById("score").innerHTML;
+
+  let eachQuestion = document.getElementById("eachQuestion");
+  eachQuestion.classList.add("hidden");
+  eachQuestion.classList.remove("shown");
+
+  let endingPage = document.getElementById("endingPage");
+  endingPage.classList.add("shown");
+  endingPage.classList.remove("hidden");
+
+  let score = document.getElementById("finalScore");
+  score.innerHTML = finalScore;
+
+  let correct = document.getElementById("correctAnswers");
+  correct.innerHTML = correctAnswers;
+
+  let total = document.getElementById("totalQuestions");
+  total.innerHTML = totalQuestions;
+
+  // Sending the quiz stats to the backend to be logged in a text file
+  let req = new XMLHttpRequest();
+  let formData = new FormData();
+
+  let params = {
+    playerScore: [finalScore],
+    correctAnswers: [correctAnswers],
+    questionsAsked: [totalQuestions],
+  };
+
+  formData.append("params", JSON.stringify(params));
+  req.open("POST", "/conclude");
+  req.send(formData);
+}
+
+// Main gameplay function
+function startGame(list) {
+  // Getting a list of all option elements, and initialising variables
+  const options = document.querySelectorAll(".answerOption");
+  let index = 0;
+  let correctAnswers = 0;
+
+  // Showing the question  page
+  let loading = document.getElementById("loading");
+  loading.classList.add("hidden");
+  loading.classList.remove("shown");
+
+  let elem = document.getElementById("eachQuestion");
+  elem.classList.add("shown");
+  elem.classList.remove("hidden");
+
+  // Showing the very first question
+  updateQuestion(list[index]);
+
+  // Adding an event listener to each option
+  options.forEach((option) => {
+    // It is a click listener
+    option.addEventListener("click", () => {
+      if (option.innerHTML === list[index][5]) {
+        // If the user gets the question right, add confetti, double the score, and add 1 to the correct answers
+        jsConfetti.addConfetti({
+          confettiRadius: 3,
+        });
+        correctAnswers++;
+        doubleScore();
+      } else {
+        // If the user gets the question wrong, reset the score — no visual feedback for now
+        resetScore();
+      }
+
+      // Increment index to move onto the next question
+      index++;
+      if (index === list.length) {
+        // If the user has answered all questions, show the ending page
+        showEndingPage(correctAnswers, list.length);
+        // Returning 0 to exit this function
+        return 0;
+      }
+
+      // Otherwise, update the question and continue
+      // An else statement is not needed as the function will exit if the condition is met
+      // Thus, if the condition is not met, the function will continue
+      updateQuestion(list[index]);
+    });
+  });
+}
